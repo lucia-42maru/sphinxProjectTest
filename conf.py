daa487c4478ba2a7d202db755fa87398e8d13cb5
dlckdgk0902@gmail.com <dlckdgk0902@gmail.com>
2022-06-09 09:40:30 +0900

    220609 sys path

--- conf.py
+++ conf.py
@@ -14,7 +14,7 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../../src'))
+sys.path.insert(0, os.path.abspath('./src'))
 
 
 # -- Project information -----------------------------------------------------
@@ -80,7 +80,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_theme_path = ['themes']
 
 # Theme options are theme-specific and customize the look and feel of a theme
