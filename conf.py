06bcfc6d0e9fd138a25b7a3c1a32da18b04d15e5
dlckdgk0902@gmail.com <dlckdgk0902@gmail.com>
2022-06-10 09:16:39 +0900

    220610 action file fix

--- conf.py
+++ conf.py
@@ -49,8 +49,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -110,6 +110,77 @@ html_static_path = []
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'Test_Project_'
 
+# -- Options for LaTeX output ------------------------------------------------
+
+# latex_elements = {
+#     # The paper size ('letterpaper' or 'a4paper').
+#     #
+#     # 'papersize': 'letterpaper',
+#
+#     # The font size ('10pt', '11pt' or '12pt').
+#     #
+#     # 'pointsize': '10pt',
+#
+#     # Additional stuff for the LaTeX preamble.
+#     #
+#     # 'preamble': '',
+#
+#     # Latex figure (float) alignment
+#     #
+#     # 'figure_align': 'htbp',
+# }
+#
+# # Grouping the document tree into LaTeX files. List of tuples
+# # (source start file, target name, title,
+# #  author, documentclass [howto, manual, or own class]).
+# latex_documents = [
+#     (master_doc, 'Test_project.tex', 'Test\\_project Documentation',
+#      'Lucia', 'manual'),
+# ]
+#
+#
+# # -- Options for manual page output ------------------------------------------
+#
+# # One entry per manual page. List of tuples
+# # (source start file, name, description, authors, manual section).
+# man_pages = [
+#     (master_doc, 'test_project', 'Test_project Documentation', [author], 1)
+# ]
+#
+#
+# # -- Options for Texinfo output ----------------------------------------------
+#
+# # Grouping the document tree into Texinfo files. List of tuples
+# # (source start file, target name, title, author,
+# #  dir menu entry, description, category)
+# texinfo_documents = [
+#     (master_doc, 'Test_project', 'Test_project Documentation',
+#      author, 'Test_project', 'One line description of project.',
+#      'Miscellaneous'),
+# ]
+#
+#
+# # -- Options for Epub output -------------------------------------------------
+#
+# # Bibliographic Dublin Core info.
+# epub_title = project
+#
+# # The unique identifier of the text. This can be a ISBN number
+# # or the project homepage.
+# #
+# # epub_identifier = ''
+#
+# # A unique identification for the text.
+# #
+# # epub_uid = ''
+#
+# # A list of files that should not be packed into the epub file.
+# epub_exclude_files = ['search.html']
+
+
+# -- Extension configuration -------------------------------------------------
+
+# -- Options for intersphinx extension ---------------------------------------
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
