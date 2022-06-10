13df3c12c0934e6962b8796469ecf14821c33a71
dlckdgk0902@gmail.com <dlckdgk0902@gmail.com>
2022-06-10 11:50:31 +0900

    220610 docs folder

--- entrypoint.sh
+++ entrypoint.sh
@@ -132,7 +132,7 @@ export ico_location
 echo "touch .nojekyll"
 touch .nojekyll
 echo "sphinx-build -E -a -b html $docs_src/$INPUT_SOURCE_DIR $docs_html -d $sphinx_doctree"
-sphinx-build -E -a -b html $docs_src/$INPUT_SOURCE_DIR $docs_html -d $sphinx_doctree
+sphinx-build -E -a -b html $docs_src/$INPUT_SOURCE_DIR $docs_html -d $sphinx_doctree/docs/source
 echo ::endgroup::
 
 # auto creation of README.md
